f83bffcfc3926f0dedf65f0a1850a6b4fc7fa928
njpearman <n.pearman@gmail.com>
2013-10-11 12:43:42 +0200

    removed generator reference

--- lib/adyen_engine/railtie.rb
+++ lib/adyen_engine/railtie.rb
@@ -2,11 +2,6 @@ require 'rails'
 
 # @private
 class AdyenEngine::Railtie < ::Rails::Railtie
-  
-  generators do
-    require 'adyen/notification_generator'
-  end
-  
   config.before_configuration do
     config.adyen = Adyen.configuration
   end
