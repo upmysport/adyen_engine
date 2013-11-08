371adfce32f64e1ddeb28f2a16fa95245326197e
njpearman <n.pearman@gmail.com>
2013-11-08 15:02:06 +0000

    implemented mapping of raw payment detail response to AR model

--- app/models/recurring_payment_details.rb
+++ app/models/recurring_payment_details.rb
@@ -1,13 +1,22 @@
+# An ActiveRecord model representing one payment detail returned by
+# Adyen::API.list_recurring_details so that the details can be locally
+# cached in the database.
+# Currently, this only represents card details, with no inheritance to
+# accommodate ELV or bank details.
 class RecurringPaymentDetails < ActiveRecord::Base
   attr_accessible :expires_on, :holder_name, :last_four_digits, :reference, :variant
 
   class << self
+    # Maps a Adyen::API::RecurringService::ListResponse to a RecurringPaymentDetails
+    # object and saves it to the database.
     def create_from!(response)
       card = response[:card]
-      expiry = Date.new(card[:expiry_year].to_i, card[:expiry_month].to_i, 1).end_of_month
+      expiry = card[:expiry_date].end_of_month
       create! reference: response[:recurring_detail_reference],
               variant: response[:variant],
-              expires_on: expiry
+              expires_on: expiry,
+              holder_name: card[:holder_name],
+              last_four_digits: card[:number]
     end
   end
 end
--- spec/models/recurring_payment_details_spec.rb
+++ spec/models/recurring_payment_details_spec.rb
@@ -5,7 +5,7 @@ describe RecurringPaymentDetails, '.create_from!' do
     response = {
         recurring_detail_reference: 'aref',
         variant: 'visa',
-        card: {expiry_month: '04', expiry_year: '2017', holder_name: 'test test', number: '4321'}
+        card: {expiry_date: Date.new(2017, 4, 1).end_of_month, holder_name: 'test test', number: '4321'}
     }
 
     RecurringPaymentDetails.create_from! response
@@ -16,4 +16,6 @@ describe RecurringPaymentDetails, '.create_from!' do
   it 'will set the expires on year' do expect(subject.expires_on.year).to eq 2017 end
   it 'will set the expires on month' do expect(subject.expires_on.month).to eq 4 end
   it 'will set the expires on day to the end of the month' do expect(subject.expires_on.day).to eq 30 end
+  it 'will set the holder name' do expect(subject.holder_name).to eq 'test test' end
+  it 'will set the card number' do expect(subject.last_four_digits).to eq '4321' end
 end
